4533c2c2637b6dd4748c331c313dc97b0cd2fddf
pirut2 <pirut.dima@yandex.ru>
2023-10-27 19:45:21 +0400

    ver_11

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,14 +20,14 @@ load_dotenv()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for product
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', default='foodgram')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
