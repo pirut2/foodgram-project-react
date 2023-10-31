d080ddbadbd2fc275b91fcc467d1e38d3484b53a
pirut2 <pirut.dima@yandex.ru>
2023-11-01 03:09:50 +0400

    ver_33

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-from rest_framework.exceptions import ValidationError
+from rest_framework.serializers import ValidationError
 from rest_framework.fields import IntegerField, SerializerMethodField
 from rest_framework.relations import PrimaryKeyRelatedField
 from rest_framework.serializers import ModelSerializer
